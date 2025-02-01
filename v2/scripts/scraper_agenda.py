5ede3e2573081208645092cf46a824ab0ba8f7ee
groaking <93555329+groaking@users.noreply.github.com>
2025-02-01 22:46:59 +0700

    Added scraper for agenda rutin

--- v2/scripts/scraper_agenda.py
+++ v2/scripts/scraper_agenda.py
@@ -16,8 +16,20 @@ def zero_pad(num):
     else:
         return str(num)
 
+# The day keys.
+day_keys_dict = {
+    "Minggu": "sun",
+    "Senin": "mon",
+    "Selasa": "tue",
+    "Rabu": "wed",
+    "Kamis": "thu",
+    "Jumat": "fri",
+    "Sabtu": "sat",
+}
+
 # The dictionary of commissions in GKI Salatiga.
 commission_dict = {
+    "MJ": "Majelis Jemaat",
     "Majelis Jemaat": "Majelis Jemaat",
     "KUL": "Komisi Usia Lanjut",
     "KD": "Komisi Dewasa",
@@ -54,37 +66,45 @@ weekday_mapping = {
 }
 
 
-class ScraperPinjamRuang(Scraper):
+class ScraperAgenda(Scraper):
     
     def __init__(self):
         super().__init__()
         
-        # Downloading the online CSV file.
+        # Downloading the online CSV files.
+        # ---
+        # "PR" stands for "Pinjam Ruang".
         url = 'https://docs.google.com/spreadsheets/d/1GAvXqBdqp1OGNtLBjZa06jPI7VCaz5JryzZAQCc4Nkk/gviz/tq?tqx=out:csv&gid=265583580'
         fi = ur.urlopen(url)
         csv = fi.read().decode('utf-8')
+        self.df_pr = pd.read_csv(io.StringIO(csv))
+        del url, fi, csv
+        # ---
+        # "AR" stands for "Agenda Rutin".
+        url = 'https://docs.google.com/spreadsheets/d/1GAvXqBdqp1OGNtLBjZa06jPI7VCaz5JryzZAQCc4Nkk/gviz/tq?tqx=out:csv&gid=1336399950'
+        fi = ur.urlopen(url)
+        csv = fi.read().decode('utf-8')
+        self.df_ar = pd.read_csv(io.StringIO(csv))
+        del url, fi, csv
+    
+    def scrape_pinjamruang_v2(self):
         
-        self.df = pd.read_csv(io.StringIO(csv))
-
-    def run(self):
-        super().run()
-
         # Preamble logging.
         print('Beginning the automation script for updating data: Peminjaman Ruangan')
 
         # Record all enumerated items.
         enumerated_items = []
         
-        for i in range(len(self.df['Timestamp'])):
-            name = self.df['Nama Kegiatan / Keperluan'][i]
+        for i in range(len(self.df_pr['Timestamp'])):
+            name = self.df_pr['Nama Kegiatan / Keperluan'][i]
             
-            t = self.df['Waktu Pemakaian Mulai dari'][i].split(':')
+            t = self.df_pr['Waktu Pemakaian Mulai dari'][i].split(':')
             time = zero_pad(t[0]) + ':' + t[1]
             
-            t = self.df['Waktu Pemakaian Sampai dengan'][i].split(':')
+            t = self.df_pr['Waktu Pemakaian Sampai dengan'][i].split(':')
             time_to = zero_pad(t[0]) + ':' + t[1]
             
-            d = self.df['Tanggal Peminjaman'][i].split('/')
+            d = self.df_pr['Tanggal Peminjaman'][i].split('/')
             d_y = d[2]
             d_m = zero_pad(d[1])
             d_d = zero_pad(d[0])
@@ -93,18 +113,18 @@ class ScraperPinjamRuang(Scraper):
             dt = datetime.date(year=int(d_y), month=int(d_m), day=int(d_d)).weekday()
             weekday = weekday_mapping[ int(dt) ]
             
-            place = self.df['Ruangan yang dipakai'][i]
+            place = self.df_pr['Ruangan yang dipakai'][i]
             
-            d = self.df['Peminjam'][i]
+            d = self.df_pr['Peminjam'][i]
             representative = commission_dict[d] if d in commission_dict.keys() else d
             
-            pic = self.df['Penanggung Jawab'][i]
+            pic = self.df_pr['Penanggung Jawab'][i]
             
-            notes = self.df['Keterangan Tambahan'][i]
+            notes = self.df_pr['Keterangan Tambahan'][i]
             if str(notes).lower() == 'nan':
                 notes = ''
             
-            approval = self.df['Persetujuan'][i]
+            approval = self.df_pr['Persetujuan'][i]
             approval = str(approval).strip()
             
             # Translate human-readable to machine-readable.
@@ -134,14 +154,76 @@ class ScraperPinjamRuang(Scraper):
             enumerated_items.append(j)
         
         self.json_data['data']['agenda-ruangan'] = copy.deepcopy(enumerated_items)
+    
+    def scrape_rutin_v2(self):
+        
+        # Preamble logging.
+        print('Beginning the automation script for updating data: Agenda Rutin')
+        
+        # Temporary dict.
+        temp_dict = {}
+        for l in list(day_keys_dict.values()):
+            temp_dict[l] = list()
+        
+        for i in range(len(self.df_ar['Hari'])):
+            name = self.df_ar['Nama Kegiatan'][i]
+            
+            t = self.df_ar['Waktu Dari'][i].split(':')
+            time = zero_pad(t[0]) + ':' + t[1]
+            
+            t = self.df_ar['Waktu Ke'][i].split(':')
+            time_to = zero_pad(t[0]) + ':' + t[1]
+            
+            d = self.df_ar['Hari'][i]
+            weekday = day_keys_dict[d]
+            
+            place = self.df_ar['Tempat'][i]
+            
+            d = self.df_ar['PIC'][i]
+            representative = commission_dict[d] if d in commission_dict.keys() else d
+            if str(representative).lower() == 'nan':
+                representative = ''
+            
+            notes = self.df_ar['Catatan Tambahan'][i]
+            if str(notes).lower() == 'nan':
+                notes = ''
+            
+            j = {
+                "name": name,
+                "time": time,
+                "time-to": time_to,
+                "timezone": "WIB",
+                "type": "rutin",
+                "place": place,
+                "representative": representative,
+                "note": notes
+            }
+            
+            # Appending the data.
+            temp_dict[weekday].append(copy.deepcopy(j))
+            del j
+        
+        # Sorting by ascending "time" order.
+        for l in list(day_keys_dict.values()):
+            temp_dict[l] = sorted(temp_dict[l], key=lambda m: m['time'])
+        
+        # Reseting the current agenda items.
+        self.json_data['data']['agenda'] = copy.deepcopy(temp_dict)
+
+    def run(self):
+        super().run()
+        
+        # Run the scrapers.
+        self.scrape_pinjamruang_v2()
+        self.scrape_rutin_v2()
         
         # Write changes.
-        super().write(write_msg='agenda-ruangan')
+        super().write(write_msg='agenda')
 
         # Finalizing the script.
         super().finish()
 
 
 if __name__ == "__main__":
-    scraper = ScraperPinjamRuang()
+    scraper = ScraperAgenda()
     scraper.run()
